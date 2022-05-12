8297285c64bab7b1dc621af468a2c437020b1686
vishal5394 <vishaljadyal@gmail.com>
2022-05-12 23:25:31 +0530

    multipletimes

--- flip_coin_simulation_problem.sh
+++ flip_coin_simulation_problem.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 
-toss=$(( RANDOM % 2 ))
-
-if(( toss -eq 0 ))
+read -p "Enter no of times to toss: " n
+heads_count=0
+tails_count=0
+for(( i = 1; i -le n; i++ ))
+do
+    toss=$(( RANDOM % 2 ))
+    if(( toss -eq 0 ))
+    then
+        echo "Flip-$i Heads"
+        (( heads_count++ ))
+    else
+        echo "Flip-$i Tails"
+        (( tails_count++ ))
+    fi 
+done
+echo "The Heads count is $heads_count and Tails Count is $tails_count"
+if(( heads_count -gt tails_count ))
 then
-    echo "Heads"
-else
-    echo "Tails"
+    echo "Winner is Heads"
+elif(( tails_count -gt heads_count ))
+then
+    echo "Winner is Tails"
+else    
+    echo "Its tie"
 fi
